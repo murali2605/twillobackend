39c6c4aa74ffb77a1c64b91b4ec094c370750b30
murali2605 <60510314+murali2605@users.noreply.github.com>
2022-08-05 19:22:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,10 @@ var port = process.env.PORT || 3000;
 const app = express();
 app.use(urlencoded({ extended: false }));
 app.post('/voice', function (req, res) {
- const { Digits, From } = req.body;
- console.log('Incoming call from ', From)
  let twiml = '';
- if (!Digits) {
-  twiml = `
-    <Response>
-      <Gather>
-        <Say>
-          Press any series of numbers on your keypad. Go nuts. You can end
-          your D T M F rampage by pressing the hash key.
-        </Say>
-      </Gather>
-    </Response>
-  `;
-} else {
-  // If Digits has been populated, repeat them back
-  twiml = `
-    <Response>
-      <Say>You entered: ${Digits}</Say>
-      <Say>Goodbye!</Say>
-    </Response>
-  `;
-}
+ <Response>
+  <Dial>+918328272581</Dial>
+</Response>
 res.type('text/xml');
 res.send(twiml)
 });
